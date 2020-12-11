c047e5fcb617434e158f7b38c41a885626ffc89a
LuoJunJie <905536460@qq.com>
2020-12-11 15:31:53 +0800

    aa

---
+++ js/good_detail.js
@@ -0,0 +1,16 @@
+var detail = document.querySelector('.detail_r_x_bh').querySelectorAll('li')
+var canshu = document.querySelectorAll('.canshu')
+for(var i=0; i<detail.length; i++) {
+    detail[i].onclick = function() {
+        for(var b=0; b<detail.length; b++) {
+            detail[b].className = ''
+            detail[b].setAttribute('index',b)
+        }
+        this.className = 'red'
+        for(var c=0; c<detail.length; c++) {
+            canshu[c].style.display = 'none'
+        }
+        var num = this.getAttribute('index')
+        canshu[num].style.display = 'block'
+    }
+}
