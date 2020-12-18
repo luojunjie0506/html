e2c84220c8b4896495b8eb1fefaee832494fd55a
LuoJunJie <905536460@qq.com>
2020-12-18 17:20:43 +0800

    555

--- js/good_detail.js
+++ js/good_detail.js
@@ -1,21 +1,23 @@
-var detail = document.querySelector('.detail_r_x_bh').querySelectorAll('li')
-var canshu = document.querySelectorAll('.canshu')
-for(var i=0; i<detail.length; i++) {
-    detail[i].onclick = function() {
-        for(var b=0; b<detail.length; b++) {
-            detail[b].className = ''
-            detail[b].setAttribute('index',b)
+window.addEventListener('load',function() {
+    var detail = document.querySelector('.detail_r_x_bh').querySelectorAll('li')
+    var canshu = document.querySelectorAll('.canshu')
+    for(var i=0; i<detail.length; i++) {
+        detail[i].onclick = function() {
+            for(var b=0; b<detail.length; b++) {
+                detail[b].className = ''
+                detail[b].setAttribute('index',b)
+            }
+            this.className = 'red'
+            for(var c=0; c<detail.length; c++) {
+                canshu[c].style.display = 'none'
+            }
+            var num = this.getAttribute('index')
+            canshu[num].style.display = 'block'
         }
-        this.className = 'red'
-        for(var c=0; c<detail.length; c++) {
-            canshu[c].style.display = 'none'
-        }
-        var num = this.getAttribute('index')
-        canshu[num].style.display = 'block'
     }
-}
-var pic = document.querySelector('.pic')
-var mask = document.querySelector('.mask')
-pic.addEventListener('mouseover',function() {
-    mask.style.display = 'block'
+    var pic = document.querySelector('.pic')
+    var mask = document.querySelector('.mask')
+    pic.addEventListener('mouseover',function() {
+        mask.style.display = 'block'
+    })
 })
