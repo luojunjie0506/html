bd4083ab59922b910c5797a1bb7c9e13120f28f2
luojunjie0506 <905536460@qq.com>
2020-12-20 12:03:53 +0800

    22

--- js/good_detail.js
+++ js/good_detail.js
@@ -17,7 +17,46 @@ window.addEventListener('load',function() {
     }
     var pic = document.querySelector('.pic')
     var mask = document.querySelector('.mask')
+    var big_box = document.querySelector('.big_box')
+    var detail_bd_1 = document.querySelector('.detail_bd_1')
+    var big_img = document.querySelector('.big_img')
     pic.addEventListener('mouseover',function() {
         mask.style.display = 'block'
+        big_box.style.display = 'block'
+
+    })
+    pic.addEventListener('mouseout',function() {
+        mask.style.display = 'none'
+        big_box.style.display = 'none'
+    })
+    pic.addEventListener('mousemove',function(e) {
+        //鼠标在pic盒子最大移动距离
+        var picXMax = pic.offsetWidth - mask.offsetWidth
+        var picYMax = pic.offsetHeight - mask.offsetHeight
+        // 鼠标在pic盒子的坐标,要让鼠标在mask遮罩层中间要减去遮罩层的一半
+        var picX = e.pageX - detail_bd_1.offsetLeft - mask.offsetWidth/2
+        var picY = e.pageY - detail_bd_1.offsetTop - mask.offsetHeight/2
+        // 限制遮罩层只能在pic盒子移动
+        if(picX <= 0 ) {
+            picX = 0
+        } else if(picX >= picXMax ) {
+            picX = picXMax
+        }
+        if(picY <= 0 ) {
+            picY = 0
+        } else if(picY >= picYMax ) {
+            picY = picYMax
+        }
+        mask.style.top = picY + 'px'
+        mask.style.left = picX + 'px'
+        
+        var bigXMax = big_img.offsetWidth - big_box.offsetWidth
+        var bigYMax = big_img.offsetHeight - big_box.offsetHeight
+        //通过小盒子的坐标/最大移动距离得到大盒子big_img中图片的移动距离
+        var bigX = picX / picXMax * bigXMax
+        var bigY = picY / picYMax * bigYMax
+        big_img.style.top = 0 - bigY + 'px'
+        big_img.style.left = 0 - bigX + 'px'
+
     })
 })
